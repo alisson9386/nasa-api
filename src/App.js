479f553d1a438c5b4d1c9dfefccd5ad3729713c7
Alisson Vieira <alisson.coura@sonda.com>
2023-05-26 15:29:27 -0300

    Update Nasa API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Universe from './Components/Universe';
+import React from "react";
+import { Canvas } from "react-three-fiber";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div style={{ height: "100vh" }}>
+	  <Universe />
+  </div>
   );
 }
 
---
+++ src/Components/Universe.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Universe extends Component {
+
+render() {
+	return (
+		<div>
+			<h1>Teste</h1>
+		</div>
+	)
+}
+}
+
+export default Universe;
