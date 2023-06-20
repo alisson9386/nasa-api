f6038c5da23fdf37c3714f3a33e171ed6a8cef6b
Alisson Vieira <alisson.coura@sonda.com>
2023-06-19 22:53:33 -0300

    Adicionando melhorias na tela

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,65 +1,68 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import NasaServices from "../Services/NasaServices";
 import Swal from 'sweetalert2';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
 
-	showLoading = (text) => {
-        Swal.fire({
-            title: 'Aguarde !',
-            html: text,// add html attribute if you want or remove
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            timerProgressBar: true,
-            didOpen: () => {
-                Swal.showLoading()
-            },
-        });
-    }
+    this.state = {
+      urlImageDay: '',
+      imageTitle: '',
+      imageDescription: '',
+      hdImage: ''
+    };
+  }
 
-	constructor(props) {
-		super(props)
+  componentDidMount() {
+    this.showLoading('Carregando');
+    NasaServices.imageDay().then((res) => {
+      this.setState({
+        urlImageDay: res.data.url,
+        imageTitle: res.data.title,
+        imageDescription: res.data.explanation,
+        hdImage: res.data.hdurl
+      });
+      this.closeLoading();
+    }).catch(error => {
+      console.log(error);
+    });
+  }
 
-		this.state = {
-			urlImageDay:'',
-			imageTilte:'',
-			imageDescription: '',
-			hdImage:''
-		}
-	}
+  showLoading = (text) => {
+    Swal.fire({
+      title: 'Aguarde!',
+      html: text,
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+      },
+    });
+  };
 
-	componentDidMount(){
-		this.showLoading('Carregando');
-		NasaServices.imageDay().then((res) =>{
-			this.setState({
-				urlImageDay: res.data.url, 
-				imageTilte: res.data.title, 
-				imageDescription: res.data.explanation,
-				hdImage: res.data.hdurl
-			});
-			Swal.close();
-		}).catch(error => {
-            console.log(error);
-          });
-	}
+  closeLoading = () => {
+    Swal.close();
+  };
 
-render() {
-	return (
-		<div className="containerHome"><br/>
-		 <div className="row">
-			<div className="col-sm-6">
-			<p className="lead">Todos os dias, a Nasa publica uma foto denominada "Image Day"</p>
-			<img className="framedImage" src={this.state.urlImageDay} alt={this.state.imageTilte} title={this.state.imageTilte}/>
-			</div>
-			<div className="col-sm-6">
-			<p className="lead">Descrição</p><br/><br/>
-			<label>{this.state.imageDescription}</label><br/><br/>
-			<a href={this.state.hdImage} target="_blank" rel="noreferrer" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Imagem em HD</a>
-			</div>
-		</div>
-		</div>
-	)
-}
+  render() {
+    return (
+      <div className="containerHome"><br />
+        <div className="row">
+          <div className="col-sm-6">
+            <p className="lead">Todos os dias, a Nasa publica uma foto denominada "Image Day"</p>
+            <img className="framedImage" src={this.state.urlImageDay} alt={this.state.imageTitle} title={this.state.imageTitle} /><br /><br />
+            <a href={this.state.hdImage} target="_blank" rel="noreferrer" className="btn btn-primary btn-sm active" role="button" aria-pressed="true">Fullscreen</a>
+          </div>
+          <div className="col-sm-6">
+            <p className="lead">Descrição</p><br /><br />
+            <label>{this.state.imageDescription}</label><br /><br />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
